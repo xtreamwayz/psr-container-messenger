6361ad349e888b567084527837316740da393312
Geert Eltink <9497586+geerteltink@users.noreply.github.com>
2020-08-25 09:16:28 +0200

    build: improve psalm config (#40)

--- src/Transport/DoctrineTransportFactory.php
+++ src/Transport/DoctrineTransportFactory.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace Xtreamwayz\PsrContainerMessenger\Transport;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManager;
 use InvalidArgumentException;
 use Psr\Container\ContainerInterface;
-use Symfony\Component\Messenger\Bridge\Doctrine\Transport\Connection;
+use Symfony\Component\Messenger\Bridge\Doctrine\Transport\Connection as TransportConnection;
 use Symfony\Component\Messenger\Bridge\Doctrine\Transport\DoctrineTransport;
 use Symfony\Component\Messenger\Exception\TransportException;
 use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
@@ -27,9 +28,15 @@ class DoctrineTransportFactory implements TransportFactoryInterface
 
     public function createTransport(string $dsn, array $options, SerializerInterface $serializer): DoctrineTransport
     {
-        $configuration = Connection::buildConfiguration($dsn, $options);
+        /**
+         * @var string[] $configuration
+         * @psalm-suppress InternalClass
+         * @psalm-suppress InternalMethod
+         */
+        $configuration = TransportConnection::buildConfiguration($dsn, $options);
 
         try {
+            /** @var Connection|EntityManager $driverConnection */
             $driverConnection = $this->container->get($configuration['connection']);
             if ($driverConnection instanceof EntityManager) {
                 $driverConnection = $driverConnection->getConnection();
@@ -42,9 +49,10 @@ class DoctrineTransportFactory implements TransportFactoryInterface
             );
         }
 
-        $connection = new Connection($configuration, $driverConnection);
+        /** @psalm-suppress InternalClass */
+        $transportConnection = new TransportConnection($configuration, $driverConnection);
 
-        return new DoctrineTransport($connection, $serializer);
+        return new DoctrineTransport($transportConnection, $serializer);
     }
 
     public function supports(string $dsn, array $options): bool
